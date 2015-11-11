74f3f05226105fc4c68a87cc360cf23afb73626e
Lusi X <lxrealty@gmail.com>
2015-11-11 00:31:46 -0500

    edited js, css; added to index

--- script.js
+++ script.js
@@ -1,21 +1,62 @@
 $(document).ready(function(){
-	$('.hello', '.tagline').css("display","none");
-	$('.hello', '.tagline').fadeIn(3000);
-	$('.hello').hover(function(){
-		$(this).css("font-size","160px");
-		$(this).css("cursor", "pointer");
-	}, function(){
-		$(this).css("font-size","150px");
-		$(this).css("cursor", "auto");	
-	});
-	$('.hello').click(function(){
-		$('.body-index').css("background","url('austin1.jpg') no-repeat 50% 50% fixed");
-		$('.body-index').css("background-size","cover");
-		$('.hello').css("display","none");
-		$('.tagline').css("display","none");
-		$('.header-index').fadeIn(2000);
-		$('#nav').fadeIn(3000);
-	});
+	var title = function(){
+        var scrollTop = $(window).scrollTop();
+        var height = $(window).height();
+        var scrollOpa = ((height - scrollTop *2) / (height));
+
+        $('.hello, .tagline, .pstar').css({
+            'opacity': scrollOpa
+        });
+    }
+    title();
+    $(window).scroll(function () {
+        title();
+    });
+
+	$('.hello,.tagline,.fa-star,.pstar').css("display","none");
+	$('.hello').fadeIn(2000);
+	$('.tagline').fadeIn(3500);
+	$('.fa-star,.pstar').fadeIn(5000);
+
+	var controller = new ScrollMagic.Controller();
+
+	var scene = new ScrollMagic.Scene({
+    	triggerElement:".about-trigger",
+    	duration:100,
+ 	})
+ 	.setTween("#animate1", {display:"initial"}) // trigger a TweenMax.to tween
+  	.addTo(controller);
+
+  	var scene = new ScrollMagic.Scene({
+    	triggerElement:".contact-trigger",
+    	duration:100,
+ 	})
+ 	.setTween("#animate2", {display:"initial"}) // trigger a TweenMax.to tween
+  	.addTo(controller);
+
+
+  // 	var scene = new ScrollMagic.Scene({
+  //   	triggerElement:".about-trigger-end"
+ 	// })
+ 	// .setTween("#animate1", {display:"none"}) // trigger a TweenMax.to tween
+  // 	.addTo(controller); 
+	// $('.hello').hover(function(){
+	// 	$(this).css("font-size","160px");
+	// 	$(this).css("cursor", "pointer");
+	// }, function(){
+	// 	$(this).css("font-size","150px");
+	// 	$(this).css("cursor", "auto");	
+	// });
+	// $('.hello').click(function(){
+	// 	$('.body-index').css("background","url('austin1.jpg') no-repeat 50% 50% fixed");
+	// 	$('.body-index').css("background-size","cover");
+	// 	$('.hello').css("display","none");
+	// 	$('.tagline').css("display","none");
+	// 	$('.header-index').fadeIn(2000);
+	// 	$('#nav').fadeIn(3000);
+	// });
+
+	
 
 
 	// $('.nav-link a').hover(function(){
