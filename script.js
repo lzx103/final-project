c8842aeb06a1b8f611d718783c4c2b61f91c31e1
Lusi X <lxrealty@gmail.com>
2015-11-11 18:04:49 -0500

    added background

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 $(document).ready(function(){
+	$('.hello,.tagline,.fa-star,.pstar,.break').css("display","none");
+	$('.hello').fadeIn(2000);
+
+	$('.hello').hover(function(){
+		$(this).css("font-size","160px");
+		$(this).css("cursor", "pointer");
+	}, function(){
+		$(this).css("font-size","150px");
+		$(this).css("cursor", "auto");	
+	});
+	$('.hello').click(function(){
+		// $('.body-index').css("background","url('austin1.jpg') no-repeat 50% 50% fixed");
+		// $('.body-index').css("background-size","cover");
+		// $('.hello').css("display","none");
+		// $('.tagline').css("display","none");
+		// $('.header-index').fadeIn(2000);
+		$('.tagline').fadeIn(2000);
+		$('.fa-star,.pstar,.break').fadeIn(3000);
+		$('#nav').fadeIn(3000);
+		$('.hello').hover(function(){
+			$(this).css("font-size","150px");
+			$(this).css("cursor", "auto");	
+		});
+	});
+	
 	var title = function(){
         var scrollTop = $(window).scrollTop();
         var height = $(window).height();
@@ -13,19 +38,33 @@ $(document).ready(function(){
         title();
     });
 
-	$('.hello,.tagline,.fa-star,.pstar').css("display","none");
-	$('.hello').fadeIn(2000);
-	$('.tagline').fadeIn(3500);
-	$('.fa-star,.pstar').fadeIn(5000);
+    var starwhite = $('.about').offset().top-500;
+  	var $w = $(window).scroll(function(){
+  		if($w.scrollTop() > starwhite){
+  			$('.fa-star').css("color","white")
+  		}
+  		else{
+  			$('.fa-star').css("color","")
+  		}
+  	});
 
 	var controller = new ScrollMagic.Controller();
-
 	var scene = new ScrollMagic.Scene({
     	triggerElement:".about-trigger",
     	duration:100,
  	})
  	.setTween("#animate1", {display:"initial"}) // trigger a TweenMax.to tween
   	.addTo(controller);
+
+  	var aboutend = $('.about-trigger-end').offset().top-100;
+  	var $w = $(window).scroll(function(){
+  		if($w.scrollTop() > aboutend){
+  			$('.about-box').css("visibility","hidden")
+  		}
+  		else{
+  			$('.about-box').css("visibility","")
+  		}
+  	});
 
   	var scene = new ScrollMagic.Scene({
     	triggerElement:".contact-trigger",
@@ -34,28 +73,70 @@ $(document).ready(function(){
  	.setTween("#animate2", {display:"initial"}) // trigger a TweenMax.to tween
   	.addTo(controller);
 
+  	var contactend = $('.contact-trigger-end').offset().top-100;
+  	var $w = $(window).scroll(function(){
+  		if($w.scrollTop() > contactend){
+  			$('.contact-box').css("visibility","hidden")
+  		}
+  		else{
+  			$('.contact-box').css("visibility","")
+  		}
+  	});
+
+  	var scene = new ScrollMagic.Scene({
+    	triggerElement:".gallery-trigger",
+    	duration:100,
+ 	})
+ 	.setTween("#animate3", {display:"initial"}) // trigger a TweenMax.to tween
+  	.addTo(controller);
+
+  	var galleryend = $('.gallery-trigger-end').offset().top;
+  	var $w = $(window).scroll(function(){
+  		if($w.scrollTop() > galleryend){
+  			$('.gallery-box').css("visibility","hidden")
+  		}
+  		else{
+  			$('.gallery-box').css("visibility","")
+  		}
+  	});
+
+  	$(".about-link").click(function() {
+        $('body').animate({
+                scrollTop: $(".about").offset().top},
+            1000);
+        return false;
+    });
+
+    $(".contact-link").click(function() {
+        $("body").animate({
+                scrollTop: $(".contact").offset().top},
+            1500);
+        return false;
+    });
+
+    $(".gallery-link").click(function() {
+        $("body").animate({
+                scrollTop: $(".gallery").offset().top},
+            1500);
+        return false;
+    });
+
+    var starend = $('.gallery-trigger-end').offset().top;
+  	var $w = $(window).scroll(function(){
+  		if($w.scrollTop() > starend){
+  			$('.fa-star').css("visibility","hidden")
+  		}
+  		else{
+  			$('.fa-star').css("visibility","")
+  		}
+  	});
 
   // 	var scene = new ScrollMagic.Scene({
   //   	triggerElement:".about-trigger-end"
  	// })
  	// .setTween("#animate1", {display:"none"}) // trigger a TweenMax.to tween
   // 	.addTo(controller); 
-	// $('.hello').hover(function(){
-	// 	$(this).css("font-size","160px");
-	// 	$(this).css("cursor", "pointer");
-	// }, function(){
-	// 	$(this).css("font-size","150px");
-	// 	$(this).css("cursor", "auto");	
-	// });
-	// $('.hello').click(function(){
-	// 	$('.body-index').css("background","url('austin1.jpg') no-repeat 50% 50% fixed");
-	// 	$('.body-index').css("background-size","cover");
-	// 	$('.hello').css("display","none");
-	// 	$('.tagline').css("display","none");
-	// 	$('.header-index').fadeIn(2000);
-	// 	$('#nav').fadeIn(3000);
-	// });
-
+	
 	
 
 
